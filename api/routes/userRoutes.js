6c2120e395d90412190d4ec780b17b85ddcd927f
BrianBoudrioux <brian1.boudrioux@epitech.eu>
2019-11-28 01:00:04 +0100

    Update userRoutes.js

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -6,6 +6,13 @@ module.exports = function(app) {
   app.route('/simplon/api/users')
     .get(user.list_all_users)
     .post(user.create_a_user);
+  
+  
+  app.route('/simplon/api/users/findBySkills')
+    .post(user.list_all_users_by_skills);
+  
+  app.route('/simplon/api/users/findByName')
+    .post(user.list_all_users_by_name);
 
 
   app.route('/simplon/api/users/:userId')
